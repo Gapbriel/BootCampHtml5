c52dae80c1e593c30e5d1b019ff7b2799b1c6f84
Gabriel Panebianco <gapbriel@gmail.com>
2012-01-08 09:49:05 -0300

    Create a Json file with your professional profile

---
+++ 8/3/4.js
@@ -0,0 +1,7 @@
+var profileJason = {
+		name: "Gaby Panebianco",
+		birthday : [20,05,1986],
+		description: "College student at Unicen",
+		experience : 'job1, job2, job3',
+		education: "Universitary",
+};
